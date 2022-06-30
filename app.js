98344b1980caf16274627cd71e307a948f1e19a5
mlarco <mlarco94@gmail.com>
2022-06-30 20:39:28 -0300

    Created an environment for Heroku

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+
+app.get("/", function (req, res){
+    res.send("Monica Larco");
+});
+
+server.listen(PORT, function() {
+  console.log('App server running');
+});
