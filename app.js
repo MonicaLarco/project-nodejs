a58c948877f7a8aed8b41bde74328086b3babf31
mlarco <mlarco94@gmail.com>
2022-07-01 11:53:50 -0300

    Changes on app.js file

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-express()
-  .get("/", (req, res) => res.send("Monica Larco"))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.get('/', (req, res) => res.send("Monica Larco"));
+
+app.listen(port, () => console.info(`Listening on ${ port }`));
+
+// express()
+//   .get("/", (req, res) => res.send("Monica Larco"))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // server.listen(PORT, function() {
 //   console.log('App server running');
